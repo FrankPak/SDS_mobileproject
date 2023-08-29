d648d823ae7185e4efa3eff9bebf8e2c81264d7d
FrankPak <francois.pakkanen@gmail.com>
2023-08-29 17:07:37 +0300

    changed layot and created CalculatorActivity

--- app/src/main/java/com/example/sds_mobileproject/MainActivity.java
+++ app/src/main/java/com/example/sds_mobileproject/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.sds_mobileproject;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    static ArrayList<String> items;
+    static ListView listView;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,22 +24,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button addBtn = (Button) findViewById(R.id.add_btn);
+        Button switchToCalc = (Button) findViewById(R.id.Calculator);
+        listView = findViewById(R.id.listView);
+        Resources res = getResources();
+
+        items = new ArrayList<>();
+        items.add("Chicken 165Kcal");
+        items.add("Pork 120Kcal");
+        items.add("Paprika 289Kcal");
+        items.add("RiceRaw 350Kcal");
+
+        adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
+
+
+
 
         addBtn.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
+
                 EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
                 EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
-                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+                //TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
 
                 int num1 = Integer.parseInt(firstNumEditText.getText().toString());
                 int num2 = Integer.parseInt(secondNumEditText.getText().toString());
                 int result = num1 + num2;
-                resultTextView.setText(result + "");
+                //resultTextView.setText(result + "");
 
             }
         });
 
+        switchToCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchActivites();
+            }
+        });
+    }
 
+    private void switchActivites() {
+        Intent switchAcitivityIntent = new Intent(this, CalculatorActivity.class);
+        startActivity(switchAcitivityIntent);
     }
 }
